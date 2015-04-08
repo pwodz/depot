45d5f9522bc8820fbdafb62d48d22c24fd7025f5
pwodz <piowod@gmail.com>
2015-04-08 19:03:55 +0200

    Catalog Display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :set_date_loaded
+  def set_date_loaded
+  	@date_loaded = Time.now
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#columns #main h1', 'Listinga Products'
+    assert_select '#columns #main table tr', minimum: 4
   end
 
   test "should get new" do
